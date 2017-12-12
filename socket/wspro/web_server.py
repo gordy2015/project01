8c98bd8c0b18e7887068a2c9e0fcff9bff16842c
root <root@master.rgkj.com>
2017-12-13 00:58:09 +0800

    wspro

---
+++ socket/wspro/web_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#coding=utf-8
+# __author__ = '戴儒锋'
+# http://www.linuxyw.com
+"""
+    执行代码前需要安装
+    pip install bottle
+    pip install websocket-client
+    pip install bottle-websocket
+"""
+# from bottle import get,run,route
+# from bottle.ext.websocket import GeventWebSocketServer
+# from bottle.ext.websocket import websocket
+from flask import Flask
+from flask.ext.socketio import SocketIO, emit
+app = Flask(__name__)
+socketio = SocketIO(app)
+users = set()   # 连接进来的websocket客户端集合
+# @get('/websocket/', apply=[websocket])
+# @app.route('/websocket/')
+@socketio.on('connect', namespace='/websocket/')
+def chat(ws):
+    users.add(ws)
+    print('add:%s'%users)
+    while True:
+        msg = ws.receive()  # 接客户端的消息
+        if msg:
+            print msg
+            for u in users:
+                u.send(msg) # 发送信息给所有的客户端
+        else:
+            print 'Not any msg'
+            break
+    # 如果有客户端断开连接，则踢出users集合
+    users.remove(ws)
+    print('remove:%s'%users)
+# run(host='0.0.0.0', port=8000, server=GeventWebSocketServer)
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0', port=8000)
